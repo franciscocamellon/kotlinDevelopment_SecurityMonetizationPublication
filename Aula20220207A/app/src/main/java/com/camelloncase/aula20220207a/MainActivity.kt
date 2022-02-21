cb8b5d8dd58b049321ecdac5aa4a03c8e1f4eb45
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-02-21 08:58:20 -0300

    Create and set a custom permission

--- Aula20220207A/app/src/main/java/com/camelloncase/aula20220207a/MainActivity.kt
+++ Aula20220207A/app/src/main/java/com/camelloncase/aula20220207a/MainActivity.kt
@@ -2,10 +2,16 @@ package com.camelloncase.aula20220207a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun callLoadImage(view: View) {}
+    fun callWriteOnSDCard(view: View) {}
+    fun callReadFromSDCard(view: View) {}
+    fun callAccessLocation(view: View) {}
 }
