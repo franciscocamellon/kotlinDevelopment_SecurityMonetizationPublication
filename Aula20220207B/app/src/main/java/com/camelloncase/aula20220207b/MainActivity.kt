f6aa8b05b8ed713cd0bfd42328af7eb516f860ba
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-02-21 08:05:59 -0300

    Initial code

---
+++ Aula20220207B/app/src/main/java/com/camelloncase/aula20220207b/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.camelloncase.aula20220207b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
