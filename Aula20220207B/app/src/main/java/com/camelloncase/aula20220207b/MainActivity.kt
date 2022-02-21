8e3b421a6a56ba6c2a6d4cbcd9c7cd48b6bd4f1f
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-02-21 08:58:57 -0300

    Call an intent using a custom permission

--- Aula20220207B/app/src/main/java/com/camelloncase/aula20220207b/MainActivity.kt
+++ Aula20220207B/app/src/main/java/com/camelloncase/aula20220207b/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.camelloncase.aula20220207b
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSecond: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSecond = findViewById(R.id.btnSecond)
+        btnSecond.setOnClickListener {
+            val i1 = Intent()
+            i1.action = "com.camelloncase.aula20220207a.Action20220207"
+            i1.addCategory("android.intent.category.DEFAULT")
+            startActivity(i1)
+        }
     }
 }
